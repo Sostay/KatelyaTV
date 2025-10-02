2f90d7ef1c335a5ee236fc1558f5cf20ab1a1ed8
Xion. <6897274+Sostay@users.noreply.github.com>
2025-10-02 16:00:44 +0800

    moon

--- proxy.worker.js
+++ proxy.worker.js
@@ -170,10 +170,11 @@ function getRootHtml() {
   <link rel="apple-touch-icon-precomposed" sizes="120x120" href="https://img.icons8.com/color/1000/kawaii-bread-1.png">
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
   <style>
       body, html {
           height: 100%;
+          max-width: 97.3%;
           margin: 0;
       }
       .background {
